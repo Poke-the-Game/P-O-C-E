6a7edadf093b0b48bcd865784bf3aadff8014502
kpj <kim.philipp.jablonski@gmail.com>
2022-11-06 00:14:14 +0100

    Add more cards

--- src/lib/card_definitions.ts
+++ src/lib/card_definitions.ts
@@ -1,4 +1,4 @@
-type ImageId = "steve" | "lon_ask"
+type ImageId = "steve" | "lon_ask" | "farmer" | "old"
 
 interface Effect {
     prosperity?: number
@@ -28,19 +28,19 @@ interface CardDefinition {
 export const cardDefinitions: Record<string, CardDefinition> = {
     inauguration: {
         name: "Secretary-General of the United Nations",
-        text: "The world is at a turning point, after an elaborate selection process, we have chosen you as our most capable advisor. Lead the way!",
+        text: "The world is at a turning point. After an elaborate selection process, we have chosen you as our most capable advisor. Lead the way!",
         image_id: "steve",
         action_description: {
             left_swipe: "Oh no...",
             right_swipe: "Right on!"
         },
         effects: {
-            "left_swipe": {},
-            "right_swipe": {}
+            left_swipe: {},
+            right_swipe: {}
         },
         permanent_effects: {
-            "left_swipe": {},
-            "right_swipe": {}
+            left_swipe: {},
+            right_swipe: {}
         }
     },
     press_day: {
@@ -60,7 +60,8 @@ export const cardDefinitions: Record<string, CardDefinition> = {
             }
         },
         permanent_effects: {
-            "left_swipe": {}, "right_swipe": {}
+            left_swipe: {},
+            right_swipe: {}
         }
     },
     coal: {
@@ -80,7 +81,8 @@ export const cardDefinitions: Record<string, CardDefinition> = {
             }
         },
         permanent_effects: {
-            "left_swipe": {}, "right_swipe": {}
+            left_swipe: {},
+            right_swipe: {}
         }
     },
     lon_ask_1: {
@@ -103,7 +105,196 @@ export const cardDefinitions: Record<string, CardDefinition> = {
             }
         },
         permanent_effects: {
-            "left_swipe": {}, "right_swipe": {}
+            left_swipe: {},
+            right_swipe: {}
         }
-    }
+    },
+    farmers: {
+        name: "A group of local farmers",
+        text: "We will all go bankrupt if we don't increase food prices.",
+        image_id: "farmer",
+        action_description: {
+            left_swipe: "No way, the population won't like it.",
+            right_swipe: "You're right, we have to do something."
+        },
+        effects: {
+            left_swipe: {
+                prosperity: -10
+            },
+            right_swipe: {
+                prosperity: -5
+            }
+        },
+        permanent_effects: {
+            left_swipe: {},
+            right_swipe: {}
+        }
+    },
+    scientists_success: {
+        name: "Diligent scientists",
+        text: "We have developed this amazing new technology which will clean our air.",
+        image_id: "old",
+        action_description: {
+            left_swipe: "I am not sure this will work...",
+            right_swipe: "Amazing, you'll be famous!"
+        },
+        effects: {
+            left_swipe: {},
+            right_swipe: {
+                prosperity: -5,
+                climate: -5
+            }
+        },
+        permanent_effects: {
+            left_swipe: {},
+            right_swipe: { climate: -1 }
+        }
+    },
+    scientists_fail: {
+        name: "Disruptive scientists",
+        text: "We have developed this amazing new technology which will clean our air.",
+        image_id: "old",
+        action_description: {
+            left_swipe: "I am not sure this will work...",
+            right_swipe: "Amazing, you'll be famous!"
+        },
+        effects: {
+            left_swipe: {},
+            right_swipe: {
+                prosperity: -15
+            }
+        },
+        permanent_effects: {
+            left_swipe: {},
+            right_swipe: {}
+        }
+    },
+    flooding: {
+        name: "Farmer from devastated area",
+        text: "Our harvest was ruined by the flood. You need to help us, or we'll starve!",
+        image_id: "farmer",
+        action_description: {
+            left_swipe: "Nah",
+            right_swipe: "Of course!"
+        },
+        effects: {
+            "left_swipe": {
+                climate: -5
+            },
+            "right_swipe": {
+                climate: 5,
+                prosperity: -10
+            }
+        },
+        permanent_effects: {
+            left_swipe: {},
+            right_swipe: {}
+        }
+    },
+    cars: {
+        name: "Lon Ask",
+        text: "You should invest in my electric car company. I'll change the world.",
+        image_id: "lon_ask",
+        action_description: {
+            left_swipe: "Yeah right...",
+            right_swipe: "Wow, I'm in!"
+        },
+        effects: {
+            left_swipe: {
+                freedom: 5
+            },
+            right_swipe: {
+                climate: 10,
+                technology: 10
+            }
+        },
+        permanent_effects: {
+            left_swipe: {},
+            right_swipe: {}
+        }
+    },
+    free_press: {
+        name: "Investigative Journalist",
+        text: "We uncovered a ploy involving several high ranking politicians to stop solar power. We have to publish this!",
+        image_id: "steve",
+        action_description: {
+            left_swipe: "No! Far too destabilizing",
+            right_swipe: "Yes, we need to know the truth"
+        },
+        effects: {
+            left_swipe: {
+                freedom: -10,
+                climate: -10
+            },
+            right_swipe: {
+                freedom: 10,
+            }
+        },
+        permanent_effects: {
+            left_swipe: {},
+            right_swipe: {}
+        }
+    },
+    taxes: {
+        name: "Economists",
+        text: "Our models predict a recession if you don't change the tax system.",
+        image_id: "old",
+        action_description: {
+            left_swipe: "Yeah nah, don't think so",
+            right_swipe: "Ah yes, quite sensible"
+        },
+        effects: {
+            left_swipe: {
+                prosperity: -5,
+            },
+            right_swipe: {
+                prosperity: 10,
+            }
+        },
+        permanent_effects: {
+            left_swipe: { prosperity: -1 },
+            right_swipe: {}
+        }
+    },
+    crypto: {
+        name: "Tech Bro",
+        text: "You definitely have to invest in this crypto coin, I just found. Think of all the NFTs you can buy!",
+        image_id: "old",
+        action_description: {
+            left_swipe: "Byeeee",
+            right_swipe: "I'm going all in!"
+        },
+        effects: {
+            left_swipe: {},
+            right_swipe: {
+                prosperity: -20,
+                freedom: 5
+            }
+        },
+        permanent_effects: {
+            left_swipe: {},
+            right_swipe: {}
+        }
+    },
+    shady: {
+        name: "Shady dude in an alley",
+        text: "Yo, take this pill, it'll make you feel great.",
+        image_id: "old",
+        action_description: {
+            left_swipe: "Maybe another time",
+            right_swipe: "Let's goooo"
+        },
+        effects: {
+            left_swipe: {
+                prosperity: -5
+            },
+            right_swipe: {
+                freedom: 10
+            }
+        },
+        permanent_effects: {
+            left_swipe: {},
+            right_swipe: {}
+        }
+    },
 }
