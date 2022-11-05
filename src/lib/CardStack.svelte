2119b3c9b9086e68f340ee40836357d452ee5c88
moritzmhmk <moritzmhmk@gmail.com>
2022-11-06 00:56:44 +0100

    Remove grayscale filter.

--- src/lib/CardStack.svelte
+++ src/lib/CardStack.svelte
@@ -110,7 +110,6 @@
     class="card"
     class:moving={offset !== 0}
     class:dropped
-    style:filter="grayscale({Math.abs(offset) > offsetThreshold ? 100 : 0}%)"
     style:transform="translateX({offset}px) translateY({dropped ? 200 : 0}vh)
     rotate({offset / 10}deg)"
     style:background="url(/card/{image}.svg)"
